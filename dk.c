a12ed7098eae38433b956e286729fa1048101435
Julian D Keller <julian.keller@wsu.edu>
2018-09-15 16:05:36 -0700

    reads in 2 floats

--- dk.c
+++ dk.c
@@ -3,15 +3,34 @@ Julian Keller
 julian.keller@wsu.edu
 Durand-Kerner Method
 
+Press ctrl+D once you are finished entering input
 */
-
-// TODO create Make File
 
 // Remember how to use debugger and visual debugger 
 
 
 #include <stdio.h>
+#include <complex.h>	// complex numbers
 #include "dk.h"	
 
-int main(){printf("HELLO WORLD\n");}
+int main(){
+
+	// TODO Readin numbers from stdin using scanf as ASCII floating point
+
+	// DO i need to use double?
+	float i ='\0'; 		// integer and decimal
+	float d ='\0'; 
+
+	// how do I want to store the coefficients?
+	// use %lf for reading in doubles
+	while(scanf("%f %f", &i, &d) == 2){
+		printf("Result %f %f\n", i, d);
+	}
+
+
+	printf("%f %f\n", i, d);
+
+
+	return 0;
+}
 
