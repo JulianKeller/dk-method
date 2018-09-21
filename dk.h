c1593afba96eced63f8f1dad9ec26650b9d522f8
Julian Keller <julian.keller@wsu.edu>
2018-09-21 15:39:33 -0700

    added more digits to PI

--- dk.h
+++ dk.h
@@ -4,13 +4,13 @@ Written by: Julian Keller
 Contact: julian.keller@wsu.edu
 Last Updated: Sept 21, 2018
 
-dk.h contains the prototypes for the functions dk.c 
+dk.h contains the prototypes for the functions dk.c
 */
 
 #ifndef DK_H
 #define DK_H
 
-#define PI 3.14159265359
+#define PI 3.14159265358979323846264338327950288
 
 double complex horner(double complex x, double complex * a, int degree);
 double complex * durandKerner(double complex * coef, double complex * z, int n);
