73e298a060f2e37357612d013725237e2cf42699
Julian D Keller <julian.keller@wsu.edu>
2018-09-21 11:03:10 -0700

    fixed iteration bug

--- dk.c
+++ dk.c
@@ -3,10 +3,14 @@ Julian Keller
 julian.keller@wsu.edu
 Durand-Kerner Method
 
-Press ctrl+D once you are finished entering input
+
+This program is used to find the roots of a polynomial using the Durand-Kerner Method. The program accepts
+complex numbers as floating point pairs. The input -5 0 represents -5.0 + 0i. This input will be the coefficient
+of a normalized polynomial. The first coefficient of 1 is not needed to be entered as it is assumed.
+
+Press ctrl+D once you are finished entering input to let the console know you have finished entering input.
 */
 
-// Remember how to use debugger and visual debugger 
 
 
 #include <stdio.h>
@@ -16,57 +20,39 @@ Press ctrl+D once you are finished entering input
 #include "dk.h"	
 
 
-/*
-From the .pdf
-R = the largest cabsolute value coefficient plus 1
-Zj = is which root I am guessing. z[0] = first root, z[1] = second root... where j=0,1...
-k = the iteration we are on
 
-*/
-
-
-// This algorithm is taken from the slides and modified for array pointers 
+// Compute the function for the given value of x using Horners Method. This algorithm is taken from 
+// the slides and modified for array pointers 
 double complex horner(double complex x, double complex * a, int degree){
 	// a contains degree+1 values
 	double complex p = a[degree];
 	for(int i = degree - 1; i >= 0; i--){
-		// printf("a[%i]: %.10f + %.10f i\n", i, creal(a[i]), cimag(a[i]));
 		p = a[i] + (x * p);
-		// printf("p: %.10f + %.10f i\n", creal(p), cimag(p));
 	}
 	return p;
 }
 
 
-
-// TODO change deltaz[j] name
-// Takes coefficient array, n number of coefficients
+// Finds the roots of the function using the Durand-Kerner Method
 double complex * durandKerner(double complex * coef, double complex * z, int n){
 
 	double complex *deltaz = calloc(20, sizeof(double complex));
-	double eps = 10e-6; 		// 10^-6 precision
-	// double complex deltaz[j] = 0;
+	double eps = 10e-7; 		// 10^-6 precision
 	double complex cmax = coef[0];
 
-	// get max abs value using cabs()
+	// get max absolute value using cabs()
 	for (int i = 1; i < n; i++){		
 		if (cabs(coef[i]) >= cabs(cmax)){	
 			cmax = cabs(coef[i]);
 		}	
 	}
 
-	
-
-	// get initial z guess values
+	// Compute initial guess values
 	double complex R = 1 + cabs(cmax);
 	for (int j = 0; j < n; j++){
-		// TODO is this the correct j???
 		double complex theta = (j * (2 * PI)/n);
 		z[j] = (R * cos(theta)) + (R * sin(theta) * I); 		// this is imaginary number I = sqrt(-1)
-		// printf("z[j]: %.10f + %.10f i\n", creal(z[j]), cimag(z[j]));
 	}
-
-	// printIterations(z, n);
 
 	
 
@@ -74,16 +60,13 @@ double complex * durandKerner(double complex * coef, double complex * z, int n){
 	for (int k = 1; k <= 50; k++){
 		double zmax = 0;
 		printf("iter %i\n", k);
+		
 		for (int j = 0; j < n; j++){
-			zmax = 0;
-			double complex diff = 0;
-			// Compute Denominator
+
+			// Compute the Denominator
 			double complex Q = 1;				
 			for (int i = 0; i < n; i++){
-				if (z[j] == z[i]){
-					// printf("Skipped z[j] == z[i]\n");
-					// printf("Skip! zj[%i]: %.10f + %.10fi, zi[%i]: %.10f + %.10fi\n",j, creal(z[j]), cimag(z[j]), i, creal(z[i]), cimag(z[i]));
-					continue;
+				if (z[j] == z[i]){continue;
 				}
 				if (Q == 0){
 					Q = (z[j] - z[i]);
@@ -93,50 +76,26 @@ double complex * durandKerner(double complex * coef, double complex * z, int n){
 				}	
 			}
 
-			// printf("Q_%i: %.10f + %.10f i\n", j, creal(Q), cimag(Q));
-			
-			// printf("z[j]: %.10f + %.10f i\n", creal(z[j]), cimag(z[j]));
-
+			// Compute the Numerator
 			complex double horn = horner(z[j], coef, n);
 
 			// deltaz[j] = f(z[j])/Qj
 			deltaz[j] = z[j] - (horn/Q);		
 
-// Correct up to here ----------------------------------------------------------------------------------
 
-			
-			// printf("deltaz[j]_%i: %.10f + %.10f i\n", j, creal(deltaz[j]), cimag(deltaz[j]));
-
-			diff = z[j] - deltaz[j];
-
-			if (cabs(diff) > zmax){
-				zmax = cabs(diff);
+			if (cabs(z[j] - deltaz[j]) > zmax){
+				zmax = cabs(z[j] - deltaz[j]);
 			}
-			// printf("zmax_%i: %.10f + %.10f i\n", j, creal(zmax), cimag(zmax));
-
-
-
-			// for (int j = 0; j < n; j++){
-			// 	printf("deltaz[j]: %.10f + %.10f i\n", creal(deltaz[j]), cimag(deltaz[j]));
-			// 	z[j] = z[j] + deltaz[j];
-			// 	// printf("z[j]: %.10f + %.10f i\n", creal(z[j]), cimag(z[j]));
-			// }
-
+			
 		}
 
 
 		printIterations(z, n);
 		for (int j = 0; j < n; j++){
-			// printf("z[j] + deltaz[j] = %.10f + %.10f i + ", creal(z[j]), cimag(z[j]));
-			// printf(" %.10f + %.10f i\n", creal(deltaz[j]), cimag(deltaz[j]));
 			z[j] = deltaz[j];
-			// printf("= z[j]: %.10f + %.10f i\n", creal(z[j]), cimag(z[j]));
 		}
 
-		
-		// FIXME zmax is increasing, not decreasing! 
-		// printf("zmax: %.10f + %.10f i\n", creal(zmax), cimag(zmax));
-		if(zmax <= eps){
+		if(eps >= zmax){
 			return z; 			
 		}
 		
@@ -148,25 +107,20 @@ double complex * durandKerner(double complex * coef, double complex * z, int n){
 
 // Method for printing each iteration
 void printIterations(double complex * z, int n){
-
-	// For now it just prints the list
 	for (int i = 0; i < n; i++){
-		printf("z[%i] = %.10f + %.10f i\n", i, creal(z[i]), cimag(z[i]));		// prints 10 digits after decimal point
+		printf("z[%i] = %.10f + %.10f i\n", i, creal(z[i]), cimag(z[i]));	// prints 10 digits after decimal point
 	}
-	// printf("\n");
 }
 
-
-
+// TODO refactor code
+// TODO check for memory leaks
 
 int main(){
 
 	int n = 0;
 	double real ='0'; 
 	double imag ='0'; 
-
-	double complex *coef = calloc(20, sizeof(double complex));		// initialize array of size 20 for coefficient of complex doubles
-
+	double complex *coef = calloc(20, sizeof(double complex));
 	double complex *z = calloc(20, sizeof(double complex));
 
 	// Read input from the user, and add complex numbers to the coefficient list
@@ -175,27 +129,21 @@ int main(){
 		n++;
 	}
 
-	// array is normalized, add 1 to the end of it
+	// array is normalized, so 1 needs to be added the end of it
 	coef[n] = 1;
 
 	durandKerner(coef, z, n);
-
-	// printIterations(coef, n);
-	// double horner(double complex x, double * a, int degree)
-	// double complex a = horner(3 + 0 * I, coef, n);
-
-	// printf("Horner :(%.1f + %.1f I)\n", creal(a), cimag(a));
-	// double complex a = 1;
-	// double complex b = 5 + 0 * I;
-	// a = a*b;
 	
-	// 	printf("nums: %.10f + %.10f i\n", creal(a), cimag(a));
-	
-
-	free(coef);		// free the memory for the coefficient array
+	// free the memory for the arrays
+	free(coef);		
 	free(z);
 
 	return 0;
 }
 
 
+// ./dk < 
+// FIXME
+// These files are stopping too early, however have the identical output for each step up until they stop
+// mult.in is different
+// p18 stops like 18 too short
